9159263c7c845a16825639a717e2fbda1fef829a
baluvamisetti <balu534@protonmail.com>
2026-06-03 10:29:44 +0530

    Clean repo - remove data, saved models and temp files

--- fix.py
+++
@@ -1,4 +0,0 @@
-content = open('train.py').read()
-content = content.replace('optimizer, mode="max", factor=0.5, patience=2, verbose=True', 'optimizer, mode="max", factor=0.5, patience=2')
-open('train.py', 'w').write(content)
-print('Fixed!')
